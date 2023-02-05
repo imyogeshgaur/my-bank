2ccecdd708cfff040fa3c065b0a39b991c433725
imyogeshgaur <yogeshgaur1995@gmail.com>
2023-02-05 10:47:08 +0530

    Added Readme File

--- js/qr.js
+++
@@ -1,5 +0,0 @@
-const genQR = () =>{
-     document.querySelector('#contact').style.display = "none";
-     document.querySelector('.secondary').style.display = "none";
-     new QRCode(document.getElementById("qr"), location.href);
-}
